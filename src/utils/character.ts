11fc098e159e332a45ee2108516c69301e83d1c9
马晓川 <724503670@qq.com>
2022-09-26 21:12:36 +0800

    update(custom):

--- src/utils/character.ts
+++ src/utils/character.ts
@@ -1,13 +1,13 @@
 /*
  * @Author: 马晓川 maxc@dustess.com
  * @Date: 2022-08-05
- * @LastEditors: 马晓川 maxc@dustess.com
- * @LastEditTime: 2022-08-08
+ * @LastEditors: 马晓川 724503670@qq.com
+ * @LastEditTime: 2022-09-26
  */
 /**
  * @description: 字符串去空格
  * @param {*} type L：左  R：右  LR：左右
- * @return {*}
+ * @return {*} 2
  */
 const strTrim = (str: string, type: 'L' | 'R' | 'LR' | 'ALL') => {
   switch (type) {
